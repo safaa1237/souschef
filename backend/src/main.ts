4605e06a1bb7584b09abdc342138c8dd87bd9f10
safaaelayoubi@gmail.com <safaaelayoubigmail.com@safaaelayoubigmailcoms-MacBook-Pro.local>
2026-03-25 09:51:23 +0700

    initial fe and dockerized be

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
 }
 bootstrap();
